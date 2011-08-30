ae8d3cb8f7c8e424d964534de0a9e0a5a1ccfea7
Vik B <vikb@bsp-a.com>
2011-08-30 16:29:29 +0100

    Fixed tweet streaming, I believe...

--- app 3.js
+++ app 3.js
@@ -18,28 +18,36 @@ var config = {
     password: "n0h4sand",
     track: ["#nodetwitter"]};
     
-var socket = require('socket.io').listen(app),
+var io = require('socket.io').listen(app),
 twitter = new (require("twitter-node").TwitterNode)(config);
 
 twitter
-    .addListener('error', function(error){ // Always check for errors or they popup client side
-                     console.log(error.message);
-                 })
-    .addListener('tweet', function(tweet){ // A new tweet that matches the criteria has been located
-                     socket.broadcast(JSON.stringify(tweet));
-                 })
-    .addListener('limit', function(limit){ // New limit has been sent from the API
-                     sys.puts('LIMIT: ' + sys.inspect(limit));
-                 })
-    .addListener('delete', function(del){ // A delete event occured
-                     sys.puts('DELETE: ' + sys.inspect(del));
-                 })
-    .addListener('end', function(resp){ // API disconnect
-                     sys.puts('wave goodbye...' + resp.statusCode);
-                 })
+    	.addListener('error', function(error){ // Always check for errors or they popup client side
+    		console.log(error.message);
+		})
+    	.addListener('tweet', function(tweet){ // A new tweet that matches the criteria has been located
+    		console.log('Got new tweet');
+			io.sockets.emit('twit', JSON.stringify(tweet));
+		})
+    	.addListener('limit', function(limit){ // New limit has been sent from the API
+			sys.puts('LIMIT: ' + sys.inspect(limit));
+		})
+    	.addListener('delete', function(del){ // A delete event occured
+    		sys.puts('DELETE: ' + sys.inspect(del));
+		})
+    	.addListener('end', function(resp){ // API disconnect
+			sys.puts('wave goodbye...' + resp.statusCode);
+		})
     .stream();
 
 // END SETUP
+
+// Config for socket.io
+io.sockets.on('connection', function(socket) {
+	socket.on('ack', function() {
+		console.log('client acknowledged twit receipt');
+	});
+});
  
 // CONTROLLERS
 app.get("/", function(req, res){
